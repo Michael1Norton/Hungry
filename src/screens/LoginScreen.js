934293d7ccb4106138c303e6ed97c11fbdc827a4
Michael1Norton <anort2002@gmail.com>
2024-03-26 14:03:37 -0400

    Fonts currently unused. Further progress to login screen.

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { SafeAreaView, View, Text, Image } from "react-native";
+
+import LoginBackground from "../images/LoginBackground.jpg";
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
+      <View style={{ paddingHorizontal: 25 }}>
+        <View style={{ alignItems: "center" }}>
+          <Image source={LoginBackground} style={{ width: 200, height: 200 }} />
+        </View>
+        <Text
+          style={{
+            fontSize: 28,
+            fontWeight: "bold",
+            color: "#120",
+            marginBottom: 35,
+          }}
+        >
+          Login
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 

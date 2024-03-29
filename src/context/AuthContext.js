fc68552f3e041ca6cde43487e318457b52ab7b64
Michael1Norton <anort2002@gmail.com>
2024-03-29 15:25:00 -0400

    Async Storage Working

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,21 +1,47 @@
 import React from "react";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const login = (token) => {
+    setIsLoading(true);
     setToken(token);
+    AsyncStorage.setItem("token", token);
+    setIsLoading(false);
   };
 
   const logout = () => {
+    setIsLoading(true);
     setToken(null);
+    AsyncStorage.removeItem("token");
+    setIsLoading(false);
   };
 
+  const isLoggedIn = async () => {
+    try {
+      setIsLoading(true);
+      let userToken = await AsyncStorage.getItem("token");
+      setToken(userToken);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(
+        "Error trying to get token from AsyncStorage for isLoggedIn: ",
+        error
+      );
+    }
+  };
+
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ token, login, logout }}>
+    <AuthContext.Provider value={{ token, login, logout, isLoading }}>
       {children}
     </AuthContext.Provider>
   );

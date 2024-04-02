e96690a8a8007b56bd91872e89443e7f3c5ead44
Michael1Norton <anort2002@gmail.com>
2024-04-02 13:12:01 -0400

    Added test data for categories and they are working in the scroll bar

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,43 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import { categoryData } from "../testData";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function Categories() {
   return (
     <View>
-      <Text>Categories</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="space-x-4"
+        contentContainerStyle={{ paddingHorizontal: 20 }}
+      >
+        {categoryData.map((category, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              className="flex item-center space-y-1"
+            >
+              <View className="rounded=full p-[6px]">
+                <Image
+                  source={{ uri: category.strCategoryThumb }}
+                  style={{ height: hp(5), width: hp(5) }}
+                  className="rounded-full"
+                />
+              </View>
+              <Text
+                style={{ fontSize: hp(1.5), textAlign: "center" }}
+                className="text-neutral-600 font-semibold"
+              >
+                {category.strCategory}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }

a92969d8bf1b696ecc438ba84262736eb97c1c44
Michael1Norton <anort2002@gmail.com>
2024-04-01 22:14:20 -0400

    Working on home screen. Remember to change AppNav back.

--- src/screens/FirstOpenScreen.js
+++ src/screens/FirstOpenScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
@@ -23,24 +23,25 @@ const FirstOpenScreen = () => {
 
     setTimeout(
       () => (ring2Padding.value = withSpring(ring2Padding.value + hp(5))),
-      100
-    );
-    setTimeout(
-      () => (ring3Padding.value = withSpring(ring3Padding.value + hp(5))),
       200
     );
     setTimeout(
-      () => (ring4Padding.value = withSpring(ring4Padding.value + hp(6))),
+      () => (ring3Padding.value = withSpring(ring3Padding.value + hp(5))),
       300
+    );
+    setTimeout(
+      () => (ring4Padding.value = withSpring(ring4Padding.value + hp(6))),
+      400
     );
   }, []);
 
   return (
-    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500 ">
       <Image
         source={require("../images/background.png")}
         style={{ position: "absolute", width: wp(100), height: hp(100) }}
       />
+
       <StatusBar style="light" />
       {/*Inserting logo with 4 rings which will be minimalistic and animated*/}
       <Animated.View
@@ -68,8 +69,13 @@ const FirstOpenScreen = () => {
         </Animated.View>
       </Animated.View>
 
-      <TouchableOpacity onPress={() => navigation.navigate("LoginScreen")}>
-        <View className="bg-amber-800 rounded-full p-4 flex items-center justify-center">
+      <TouchableOpacity
+        onPress={() => {
+          console.log("Button pressed");
+          navigation.navigate("LoginScreen");
+        }}
+      >
+        <View className="bg-amber-800 rounded-full p-4 items-center justify-center">
           <Text className="font-bold text-white text-lg">Lets Eat! </Text>
         </View>
       </TouchableOpacity>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,27 +1,81 @@
 import React from "react";
-import { ScrollView } from "react-native";
-import { SafeAreaView, Text, StyleSheet, Button } from "react-native";
-import { AuthContext } from "../context/AuthContext";
+import { View, Text, ScrollView, Image, TextInput } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons"; // Import MaterialIcons
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 50 }}
+        contentContainerStyle={{ paddingBottom: 50, paddingTop: hp(3) }}
+        className="space-y-6 pt-15"
       >
-        <Text>Landing Screen :D Awesome</Text>
+        {/* Inserting the user icon */}
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../images/profilePic.png")}
+            style={{ height: hp(6), width: hp(5), borderRadius: hp(5) }}
+          />
+          <MaterialIcons
+            name="notifications"
+            size={hp(4)}
+            color="black"
+            style={{ marginRight: 10 }}
+          />
+        </View>
+
+        {/* Welcome Back */}
+        <View className="mx-4 space-y-2 mb-2">
+          <Text style={{ fontSize: hp(2) }} className="text-neutral-600">
+            Welcome back!
+          </Text>
+
+          {/* Make ur own food and the home color */}
+          <View>
+            <Text
+              style={{ fontSize: hp(4) }}
+              className="font-semibold text-neutral-600"
+            >
+              Make your own food,
+            </Text>
+          </View>
+          <Text
+            style={{ fontSize: hp(4) }}
+            className="font-semibold text-neutral-600"
+          >
+            stay at <Text className="text-amber-400">home</Text>
+          </Text>
+        </View>
+
+        {/* Search Bar */}
+        <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+          {/* Search icon */}
+          <MaterialIcons
+            name="search"
+            size={24}
+            color="black"
+            style={{ marginRight: 10 }}
+          />
+          {/* TextInput */}
+          <TextInput
+            placeholder="Search for recipes"
+            placeholderTextColor={"gray"}
+            style={{ fontSize: hp(2), flex: 1 }}
+            className="text-base mb-1 pl-3 tracking-wider"
+          />
+        </View>
+
+        {/* Categories */}
+        <View></View>
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default HomeScreen;

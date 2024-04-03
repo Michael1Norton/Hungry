e07462e6bf5b66a8664b62f9927b31fd05a58c2a
Michael1Norton <anort2002@gmail.com>
2024-04-03 16:35:28 -0400

    Beginning updates to UserProfileScreen

--- src/screens/UserProfileScreen.js
+++ src/screens/UserProfileScreen.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  Button,
+  SafeAreaView,
+} from "react-native";
 import { AuthContext } from "../context/AuthContext";
-import { useNavigation } from "@react-navigation/native"; // Import useNavigation hook
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { useNavigation } from "@react-navigation/native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const UserProfileScreen = () => {
   const { logout } = React.useContext(AuthContext);
@@ -16,20 +28,51 @@ const UserProfileScreen = () => {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Landing Screen :D</Text>
-      <Button title="Logout" onPress={handleLogout} />
-      <Button title="Go Back" onPress={handleGoBack} />
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#F5CCA0" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingHorizontal: hp(2),
+          paddingTop: hp(4),
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{ backgroundColor: "white", borderRadius: 20 }}
+        >
+          <MaterialIcons name="chevron-left" size={hp(4)} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: hp(1.5),
+        }}
+      >
+        <Image
+          source={require("../images/profilePic.png")}
+          style={{ height: hp(22), width: hp(22), borderRadius: hp(5) }}
+        />
+        <Button title="Sign Out" onPress={handleLogout} />
+      </View>
+
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text>View Your Favorite Recipes:</Text>
+        <View
+          style={{
+            height: 2,
+            backgroundColor: "black",
+            width: "100%",
+            marginTop: hp(0.5),
+            marginBottom: hp(1.5),
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default UserProfileScreen;
